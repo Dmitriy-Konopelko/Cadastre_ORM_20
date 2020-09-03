d9f2d4646eaca2dba5ba4826c82413b4da94f6ff
Dmitriy-Konopelko <52877290+Dmitriy-Konopelko@users.noreply.github.com>
2020-09-03 21:21:55 +0300

    создали базовый класс команд

--- Cadastre_ORM_20/ViewModels/MainWindowViewModel.cs
+++ Cadastre_ORM_20/ViewModels/MainWindowViewModel.cs
@@ -28,8 +28,22 @@ namespace Cadastre_ORM_20.ViewModels
             //}
             // 3 вариант присвоения
             set => Set(ref _Title, value);
-
         }
         #endregion
+
+        #region Статус програмы
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+        /// <summary>Статус программы</summary>
+        /// 
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+        #endregion
+
+
+
     }
 }
