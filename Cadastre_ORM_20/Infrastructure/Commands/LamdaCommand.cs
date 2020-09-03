0d69899bf94350a2926cd9b543a0027e9ed0786f
Dmitriy-Konopelko <52877290+Dmitriy-Konopelko@users.noreply.github.com>
2020-09-03 21:37:32 +0300

    Создали главный класс команды для нашего приложения

---
+++ Cadastre_ORM_20/Infrastructure/Commands/LamdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using Cadastre_ORM_20.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastre_ORM_20.Infrastructure.Commands
+{
+    internal class LamdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LamdaCommand(Action<Object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException (nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
