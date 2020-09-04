a77fc49f24875e5fd1ade932f3cf878f48519f1f
Dmitriy-Konopelko <52877290+Dmitriy-Konopelko@users.noreply.github.com>
2020-09-04 22:16:04 +0300

    Устанавливаем текущее значение региональных настроек в русский регион, для корректного отображения даты и времени

--- Cadastre_ORM_20/App.xaml.cs
+++ Cadastre_ORM_20/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace Cadastre_ORM_20
 {
@@ -13,5 +16,16 @@ namespace Cadastre_ORM_20
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //Устанавливаем текущее значение региональных настроек в русский регион, для корректного отображения даты и времени
+            var cultureInfo = new CultureInfo("ru-RU");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+            base.OnStartup(e);
+        }
     }
 }
